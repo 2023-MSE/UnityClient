107bc3c2de99c3c863bd0bf284458cb692cc14c4
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2023-04-10 18:27:56 +0900

    Make login scene

---
+++ MSEProject/Assets/Scripts/LoginScriptFolder/SigninupResult.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SigninupResult
+{
+    SUCCESS = 0,
+    NETWORK_ERROR = 1,
+    INVALID_ID,
+    INVALID_PASSWD,
+    INVALID_NICKNAME
+}

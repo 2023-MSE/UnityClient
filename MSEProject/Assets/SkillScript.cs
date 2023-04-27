325e1addf3b7ef07e56fc4dfb0400dc364bbf548
Yumi jun <junyumi1006@naver.com>
2023-04-27 17:46:16 +0900

    skill-update
    push 3skill..add more

---
+++ MSEProject/Assets/SkillScript.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillScript : Skill
+{
+
+    public int type = 0;
+
+    public int damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
